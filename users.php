c4d9f42eb451b70594d4693a37ca1c4131238525
Giavonator <gioalvez33@gmail.com>
2024-05-14 22:03:06 -0400

    Start chat php

--- users.php
+++ users.php
@@ -1,19 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Realtime Chat Application</title>
-	<link rel="stylesheet" href="style.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-</head>
+<?php 
+	session_start();
+	if (!isset($_SESSION['unique_id'])){
+		header('location: login.php');
+	}
+?>
 
 
+<?php include "header.php";?>
 <body>
 	<div class="wrapper">
 		<section class="users">
 			<header>
+			<?php
+	    		include_once "php/config.php";
+				$sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = ");
+	
+			?>
 				<div class="content">
 					<img src="img.jpg" alt="">
 					<div class="details">
